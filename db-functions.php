606e5f656017c19da3ad0f37859824a5b0b1250f
Ta1Vos <302196386@student.rocmondriaan.nl>
2024-01-17 14:06:17 +0100

    removed accidental html page

--- db-functions.php
+++ db-functions.php
@@ -67,24 +67,3 @@ function fetchFromDbUsingCity(string $city):array|false {
 
     return false;
 }
-
-?>
-
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<style>
-    table, tr, td {
-        border: 1px solid black;
-    }
-</style>
-<body>
-
-</body>
-</html>
